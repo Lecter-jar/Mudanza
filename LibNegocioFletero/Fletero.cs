071b6fc8a0c43e630fc2b0681d97278efd67bfff
Lecter <leandro_valenzuela@hotmail.com>
2019-06-29 18:19:51 -0400

    Agregar archivos de proyecto.

---
+++ LibNegocioFletero/Fletero.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace LibNegocioFletero
+{
+    public class Fletero
+    {
+
+        #region Atributos
+        string _mensaje;
+        bool _exito = false;
+        string _rut;
+        string _primerNombre;
+        string _segundoNombre;
+        string _apellidoPaterno;
+        string _apellidoMaterno;
+        DateTime _fechaNacimiento;
+        string _direccion;
+        int _comuna;
+        string _fono;
+        string _email;
+        DataSet _ds = new DataSet();
+        int _tipoVehiculo;
+        #endregion
+
+
+
+
+
+
+
+
+
+    } // fin class
+
+} // fin namespace
