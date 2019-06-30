275e09aa3a51dbef414e280c41981c77f0339369
Lecter <leandro_valenzuela@hotmail.com>
2019-06-29 21:19:06 -0400

    ComunaClass

---
+++ LibNegocioFletero/Comuna.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace LibNegocioFletero
+{
+    public class Comuna
+    {
+
+        #region Atributos
+        string _mensaje;
+        bool _exito = false;
+        int _id;
+        string _nombre;
+        DataSet _ds = new DataSet();
+        #endregion
+
+        #region Propiedades
+        public string Mensaje
+        {
+            get
+            {
+                return _mensaje;
+            }
+
+            set
+            {
+                _mensaje = value;
+            }
+        }
+
+        public bool Exito
+        {
+            get
+            {
+                return _exito;
+            }
+
+            set
+            {
+                _exito = value;
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+
+            set
+            {
+                _nombre = value;
+            }
+        }
+
+        public DataSet Ds
+        {
+            get
+            {
+                return _ds;
+            }
+
+            set
+            {
+                _ds = value;
+            }
+        }
+
+        #endregion
+
+    }
+}
